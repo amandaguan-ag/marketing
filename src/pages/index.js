c5f00c1cdca75a16a317688ca1bdba87de8bc7ee
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-24 16:51:17 -0700

    Add styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className="card">
+      <div className="card-header">
+        <div className="avatar">
           <Image
             src="/profile-thumbnail.png" 
             alt="Profile picture of Sarah Dole"
             width={48}
             height={48}
+            className="avatar-image"
           />
         </div>
-        <div>
-          <span>Sarah Dole</span>
-          <span>@sarahdole</span>
+        <div className="user-info">
+          <span className="user-name">Sarah Dole</span>
+          <span className="user-handle">@sarahdole</span>
         </div>
       </div>
-      <div>
-        <span>
+      <div className="card-content">
+        <span className="card-text">
           I've been searching for high-quality abstract images for my design
           projects, and I'm thrilled to have found this platform. The variety
           and depth of creativity are astounding!
