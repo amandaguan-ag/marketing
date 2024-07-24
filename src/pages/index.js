656719f6de7af500840c27c7a73bec97766b8d0e
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-24 16:55:10 -0700

    position in page center

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,33 @@
-import Image from "next/image"; 
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="card">
-      <div className="card-header">
-        <div className="avatar">
-          <Image
-            src="/profile-thumbnail.png" 
-            alt="Profile picture of Sarah Dole"
-            width={48}
-            height={48}
-            className="avatar-image"
-          />
+    <div className="page-container">
+      <div className="content-container">
+        <div className="card">
+          <div className="card-header">
+            <div className="avatar">
+              <Image
+                src="/profile-thumbnail.png"
+                alt="Profile picture of Sarah Dole"
+                width={48}
+                height={48}
+                className="avatar-image"
+              />
+            </div>
+            <div className="user-info">
+              <span className="user-name">Sarah Dole</span>
+              <span className="user-handle">@sarahdole</span>
+            </div>
+          </div>
+          <div className="card-content">
+            <span className="card-text">
+              I've been searching for high-quality abstract images for my design
+              projects, and I'm thrilled to have found this platform. The
+              variety and depth of creativity are astounding!
+            </span>
+          </div>
         </div>
-        <div className="user-info">
-          <span className="user-name">Sarah Dole</span>
-          <span className="user-handle">@sarahdole</span>
-        </div>
-      </div>
-      <div className="card-content">
-        <span className="card-text">
-          I've been searching for high-quality abstract images for my design
-          projects, and I'm thrilled to have found this platform. The variety
-          and depth of creativity are astounding!
-        </span>
       </div>
     </div>
   );
