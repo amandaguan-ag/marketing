51e4076fcf42afa29ef62b6084ce34b25f9d00e2
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-24 17:37:51 -0700

    Pass the testimonial content as props

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import PropTypes from "prop-types";
 
-const TestimonialCard = () => {
+const TestimonialCard = ({ author, handle, blurb, imageSrc, imageAlt }) => {
   return (
     <div className="h-[233px] w-[340px] flex flex-col justify-center gap-4 bg-white p-6 rounded-lg border border-solid border-neutral-200 shadow-md">
       <div className="flex justify-center items-center gap-4 w-full">
         <div className="w-12 h-12">
           <Image
-            src="/profile-thumbnail.png"
-            alt="Profile picture of Sarah Dole"
+            src={imageSrc}
+            alt={imageAlt}
             width={48}
             height={48}
             className="w-12 h-12 object-cover rounded-full"
@@ -15,22 +16,24 @@ const TestimonialCard = () => {
         </div>
         <div className="flex flex-col gap-1 flex-grow">
           <span className="font-semibold text-lg text-neutral-900">
-            Sarah Dole
+            {author}
           </span>
-          <span className="font-normal text-sm text-neutral-600">
-            @sarahdole
-          </span>
+          <span className="font-normal text-sm text-neutral-600">{handle}</span>
         </div>
       </div>
       <div className="flex justify-center gap-2 w-full rounded-lg">
-        <span className="font-normal text-base text-neutral-600">
-          I've been searching for high-quality abstract images for my design
-          projects, and I'm thrilled to have found this platform. The variety
-          and depth of creativity are astounding!
-        </span>
+        <span className="font-normal text-base text-neutral-600">{blurb}</span>
       </div>
     </div>
   );
+};
+
+TestimonialCard.propTypes = {
+  author: PropTypes.string.isRequired,
+  handle: PropTypes.string.isRequired,
+  blurb: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
 };
 
 export default TestimonialCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
       <div className="flex flex-col items-center">
-        <TestimonialCard />
+        <TestimonialCard
+          author="Sarah Dole"
+          handle="@sarahdole"
+          blurb="I've been searching for high-quality abstract images for my design
+          projects, and I'm thrilled to have found this platform. The variety
+          and depth of creativity are astounding!"
+          imageSrc="/profile-thumbnail.png"
+          imageAlt="Headshot of Sarah Dole"
+        />
       </div>
     </div>
   );
