df92ebdb5ed94f8433f9972182d567c7b96e3f2d
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-24 17:34:37 -0700

    Apply Tailwind css

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,26 +1,29 @@
 import Image from "next/image";
-import styles from "../styles/TestimonialCard.module.css";
 
 const TestimonialCard = () => {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardHeader}>
-        <div className={styles.avatar}>
+    <div className="h-[233px] w-[340px] flex flex-col justify-center gap-4 bg-white p-6 rounded-lg border border-solid border-neutral-200 shadow-md">
+      <div className="flex justify-center items-center gap-4 w-full">
+        <div className="w-12 h-12">
           <Image
             src="/profile-thumbnail.png"
             alt="Profile picture of Sarah Dole"
             width={48}
             height={48}
-            className={styles.avatarImage}
+            className="w-12 h-12 object-cover rounded-full"
           />
         </div>
-        <div className={styles.userInfo}>
-          <span className={styles.userName}>Sarah Dole</span>
-          <span className={styles.userHandle}>@sarahdole</span>
+        <div className="flex flex-col gap-1 flex-grow">
+          <span className="font-semibold text-lg text-neutral-900">
+            Sarah Dole
+          </span>
+          <span className="font-normal text-sm text-neutral-600">
+            @sarahdole
+          </span>
         </div>
       </div>
-      <div className={styles.cardContent}>
-        <span className={styles.cardText}>
+      <div className="flex justify-center gap-2 w-full rounded-lg">
+        <span className="font-normal text-base text-neutral-600">
           I've been searching for high-quality abstract images for my design
           projects, and I'm thrilled to have found this platform. The variety
           and depth of creativity are astounding!
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import TestimonialCard from "../components/TestimonialCard";
 
 export default function Home() {
   return (
-    <div className="background">
-      <div className="content-container">
+    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
+      <div className="flex flex-col items-center">
         <TestimonialCard />
       </div>
     </div>
