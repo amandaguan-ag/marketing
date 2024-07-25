d2f778006bfee8c6de3ffc09971c8f31965c47a0
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-25 14:33:41 -0700

    reposition to design

--- src/pages/testimonial-card.js
+++ src/pages/testimonial-card.js
@@ -2,18 +2,16 @@ import TestimonialCard from "../components/TestimonialCard";
 
 export default function TestimonialCardPage() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
-      <div className="flex flex-col items-center custom-margin-768 custom-margin-375 custom-margin-default">
-        <TestimonialCard
-          author="Sarah Dole"
-          handle="@sarahdole"
-          blurb="I've been searching for high-quality abstract images for my design
-          projects, and I'm thrilled to have found this platform. The variety
-          and depth of creativity are astounding!"
-          imageSrc="/profile-thumbnail.png"
-          imageAlt="Headshot of Sarah Dole"
-        />
-      </div>
+    <div className="container">
+      <TestimonialCard
+        author="Sarah Dole"
+        handle="@sarahdole"
+        blurb="I've been searching for high-quality abstract images for my design
+        projects, and I'm thrilled to have found this platform. The variety
+        and depth of creativity are astounding!"
+        imageSrc="/profile-thumbnail.png"
+        imageAlt="Headshot of Sarah Dole"
+      />
     </div>
   );
 }
