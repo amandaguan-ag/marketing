bba15f331f19bcb7f74ef89935c69029637e7a6c
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-24 17:50:57 -0700

    chore: Refactor folder structure and add TestimonialCard page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
-import TestimonialCard from "../components/TestimonialCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
-      <div className="flex flex-col items-center">
-        <TestimonialCard
-          author="Sarah Dole"
-          handle="@sarahdole"
-          blurb="I've been searching for high-quality abstract images for my design
-          projects, and I'm thrilled to have found this platform. The variety
-          and depth of creativity are astounding!"
-          imageSrc="/profile-thumbnail.png"
-          imageAlt="Headshot of Sarah Dole"
-        />
-      </div>
-    </div>
+    <main className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
+      <ul className="list-none">
+        <li className="mb-4">
+          <Link href="/testimonial-card">Testimonial Card</Link>
+        </li>
+      </ul>
+    </main>
   );
 }
