1b3469d4d157352a00ee19bbe392244ec5eb5033
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-25 16:23:09 -0700

    Take care of text overflow and update style

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,29 +1,34 @@
 import Image from "next/image";
 import PropTypes from "prop-types";
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({ subsets: ["latin"] });
 
 const TestimonialCard = ({ author, handle, blurb, imageSrc, imageAlt }) => {
   return (
-    <div className="h-[233px] w-[340px] flex flex-col justify-center gap-4 bg-white p-6 rounded-lg ring-1 ring-neutral-200 shadow-md">
-      <div className="flex justify-center items-center gap-4 w-full">
-        <div className="w-12 h-12">
-          <Image
-            src={imageSrc}
-            alt={imageAlt}
-            width={48}
-            height={48}
-            className="w-12 h-12 object-cover rounded-full"
-          />
-        </div>
-        <div className="flex flex-col gap-1 flex-grow">
-          <span className="font-semibold text-lg text-neutral-900">
+    <div
+      className={`${notoSans.className} bg-white p-6 rounded-lg w-full max-w-md text-neutral-600 flex flex-col gap-4 shadow-md outline outline-1 outline-neutral-200`}
+    >
+      <div className="flex items-center gap-4">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+        <div className="flex flex-col gap-1 overflow-hidden">
+          <p className="text-lg text-neutral-900 font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
             {author}
-          </span>
-          <span className="font-normal text-sm text-neutral-600">{handle}</span>
+          </p>
+          <p className="text-sm text-neutral-600 whitespace-nowrap overflow-hidden text-ellipsis">
+            {handle}
+          </p>
         </div>
       </div>
-      <div className="flex justify-center gap-2 w-full rounded-lg">
-        <span className="font-normal text-base text-neutral-600">{blurb}</span>
-      </div>
+      <p className="text-neutral-600 text-base overflow-hidden text-ellipsis">
+        {blurb}
+      </p>
     </div>
   );
 };
