6c713f376b2276069e5ae4c67480de30d4350609
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-26 15:20:23 -0700

    add a horizontally-centered card that is a fixed distance of 200px from the top of the page.

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,7 +1,16 @@
 const ProfileCard = () => {
-  return;
+  return (
+    <section
+      className="bg-white px-4 py-6 rounded-lg w-[340px] flex flex-col shadow-md mx-auto mt-[200px]"
+      role="complementary"
+    >
+      {/* Add your content here */}
+    </section>
+  );
 };
 
-ProfileCard.propTypes = {};
+ProfileCard.propTypes = {
+  // Define your prop types if needed
+};
 
 export default ProfileCard;
