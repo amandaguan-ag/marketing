ed773ec2d9660a640b8d0f97b11fb239a4b6f746
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-26 11:59:57 -0700

    Align blog to style guide

--- src/pages/blog-card.js
+++ src/pages/blog-card.js
@@ -3,11 +3,11 @@ import BlogCard from "../components/BlogCard";
 export default function BlogCardPage() {
   return (
     <main className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-50 to-[#d2d6db]">
-      <div className="mt-[120px] mx-[120px]">
+      <div className="mt-[120px] mx-auto max-w-[1280px] px-8">
         <BlogCard
           title="Top 5 Living Room Inspirations"
           category="Interior"
-          description="Curated vibrant colors for your living, make it pop & calm at the same time."
+          description="Curated vibrants colors for your living, make it pop & calm in the same time."
           imageSrc="/spacejoy-YqFz7UMm8qE-unsplash.jpg"
         />
       </div>
