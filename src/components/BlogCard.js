4fff551ad4c9ce9eeae18b5e0ebe260148d059c7
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-25 17:15:24 -0700

    Align blog category styling

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -11,17 +11,23 @@ const BlogCard = ({ title, category, description, imageSrc }) => {
         className="self-stretch h-72 object-cover rounded-t-lg"
       />
       <div className="flex flex-col gap-3 self-stretch px-4 py-6">
-        <div className="flex flex-col gap-2 self-stretch">
-          <div className="flex items-center bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200">
+        <div className="flex flex-col gap-2 self-stretch relative">
+          <div
+            className="flex items-center bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 absolute"
+            style={{ top: "0px", left: "0px" }}
+          >
             <span className="font-normal text-sm text-center text-green-700">
               {category}
             </span>
           </div>
-          <span className="font-semibold text-lg text-neutral-900">
+          <span className="font-semibold text-lg text-neutral-900 mt-8">
             {title}
           </span>
         </div>
-        <div className="flex flex-col gap-6 self-stretch">
+        <div
+          className="flex flex-col gap-6 self-stretch mt-auto"
+          style={{ paddingBottom: "36px" }}
+        >
           <span className="font-medium text-base text-neutral-600">
             {description}
           </span>
