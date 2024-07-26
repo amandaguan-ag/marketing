3a22683f85489051f8c263eb487749fb114d99cf
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-26 15:57:18 -0700

    add aria-label for index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,24 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center from-gray-50 to-[#d2d6db]">
-      <nav>
+      <nav aria-label="Main navigation">
         <ul className="list-none">
           <li className="mb-4">
-            <Link href="/testimonial-card">Testimonial Card</Link>
+            <Link href="/testimonial-card">
+              <a aria-label="Navigate to Testimonial Card page">
+                Testimonial Card
+              </a>
+            </Link>
           </li>
           <li className="mb-4">
-            <Link href="/blog-card">Blog Card</Link>
+            <Link href="/blog-card">
+              <a aria-label="Navigate to Blog Card page">Blog Card</a>
+            </Link>
           </li>
           <li className="mb-4">
-            <Link href="/profile-card">Profile Card</Link>
+            <Link href="/profile-card">
+              <a aria-label="Navigate to Profile Card page">Profile Card</a>
+            </Link>
           </li>
         </ul>
       </nav>
