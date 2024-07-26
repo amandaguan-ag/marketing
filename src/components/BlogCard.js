e81d1073c5be94304022d9a141eaefdb99e3cf05
amandaguan-ag <amandaguan1314@gmail.com>
2024-07-25 21:05:43 -0700

    Improve accessibility

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,42 +3,44 @@ import { RiArrowRightLine } from "@remixicon/react";
 
 const BlogCard = ({ title, category, description, imageSrc }) => {
   return (
-    <div className="w-[340px] flex flex-col bg-white rounded-lg shadow-md border border-neutral-200">
+    <article className="w-[340px] flex flex-col bg-white rounded-lg shadow-md border border-neutral-200">
       <Image
         src={imageSrc}
-        alt={title}
+        alt={`${title} image`}
         width={340}
         height={288}
         className="self-stretch h-72 object-cover rounded-t-lg"
-        preload={true}
       />
       <div className="flex flex-col gap-3 self-stretch px-4 py-6">
-        <div className="flex flex-col gap-2 self-stretch relative">
-          <div className="flex items-center bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 absolute top-0 left-0">
+        <header className="flex flex-col gap-2 self-stretch relative">
+          <div
+            className="flex items-center bg-green-50 px-2 py-0.5 rounded-full border border-solid border-green-200 absolute top-0 left-0"
+            aria-label={`Category: ${category}`}
+          >
             <span className="font-normal text-sm text-center text-green-700">
               {category}
             </span>
           </div>
-          <span className="font-semibold text-lg text-neutral-900 mt-8">
+          <h2 className="font-semibold text-lg text-neutral-900 mt-8">
             {title}
-          </span>
-        </div>
-        <div
-          className="flex flex-col gap-6 self-stretch mt-auto relative"
+          </h2>
+        </header>
+        <p className="font-medium text-base text-neutral-600">{description}</p>
+        <footer
+          className="flex justify-start items-center gap-1.5 mt-auto relative"
           style={{ paddingBottom: "36px" }}
         >
-          <span className="font-medium text-base text-neutral-600">
-            {description}
-          </span>
-          <div className="flex justify-start items-center gap-1.5 absolute bottom-0 left-0 px-0.5">
-            <span className="font-medium text-base text-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-              Read more
-            </span>
+          <a
+            href="#"
+            className="flex justify-start items-center gap-1.5 px-0.5 text-indigo-700 font-medium text-base focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            aria-label={`Read more about ${title}`}
+          >
+            <span>Read more</span>
             <RiArrowRightLine className="w-5 h-5 text-indigo-700" />
-          </div>
-        </div>
+          </a>
+        </footer>
       </div>
-    </div>
+    </article>
   );
 };
 
